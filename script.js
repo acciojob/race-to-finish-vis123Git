9f8c5cbe71c7856ac5cbdfd9f296519d8dc7e1b8
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2023-10-27 08:27:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 window.promises = [];
 
-// Do not change the code above this
-// add your promises to the array `promises`
+
+const minDelay = 1000; // 1 second
+const maxDelay = 5000; // 5 seconds
+
+function createRandomDelayPromise() {
+    const randomDelay = Math.floor(Math.random() * (maxDelay - minDelay + 1)) + minDelay;
+
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`Promise resolved after ${randomDelay / 1000} seconds`);
+        }, randomDelay);
+    });
+}
+
+for (let i = 0; i < 5; i++) {
+    const promise = createRandomDelayPromise();
+    window.promises.push(promise);
+}
+
